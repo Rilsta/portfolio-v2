14bd49858ce49e774c4c47551b560971a09e939b
rilsta <rstarnes9@gmail.com>
2016-05-29 13:55:44 -0700

    add jquery hide and show functions for portfolio items

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
-  $("#box").click( function () {
-    $("div").toggleClass("bigger");
-    console.log('gets here');
+
+  $(".m-box").addClass("m-box");
+
+  $("#design").click(function() {
+    $("#design-content").toggleClass("dropdown");
+    $("#develop, #delight, #logo, .empty").toggle("hidden");
   });
+  $("#develop").click(function() {
+    $("#develop-content").toggleClass("dropdown");
+    $("#design, #delight, #logo, .empty").toggle("hidden");
+  });
+  $("#delight").click(function() {
+    $("#delight-content").toggleClass("dropdown");
+    $("#develop, #design, #logo, .empty").toggle("hidden");
+  });
+  // Animations
+  // $("#box-container").addClass("m-scene");
+  // $("#logo").addClass("scene__element scene__element--fadeinup");
 });
