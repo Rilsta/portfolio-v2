f2d539e24e2c1b6caf9f7ca21a88642d1bc3dfae
rilsta <rstarnes9@gmail.com>
2016-05-29 14:28:08 -0700

    fix slide toggle in jquery on main page

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@ $(document).ready(function() {
 
   $(".m-box").addClass("m-box");
 
+  //Hidden on page load
+  $("#design-content").addClass("dropdown hidden");
+
+  //Dropdown hide/show for main menu
   $("#design").click(function() {
-    $("#design-content").toggleClass("dropdown");
-    $("#develop, #delight, #logo, .empty").toggle("hidden");
+    $("#design-content, #develop, #delight, #logo, .empty").slideToggle("hidden");
   });
-  $("#develop").click(function() {
-    $("#develop-content").toggleClass("dropdown");
-    $("#design, #delight, #logo, .empty").toggle("hidden");
-  });
-  $("#delight").click(function() {
-    $("#delight-content").toggleClass("dropdown");
-    $("#develop, #design, #logo, .empty").toggle("hidden");
-  });
+  // $("#develop").click(function() {
+  //   $("#develop-content").toggleClass("dropdown");
+  //   $("#design, #delight, #logo, .empty").slideToggle("hidden");
+  // });
+  // $("#delight").click(function() {
+  //   $("#delight-content").toggleClass("dropdown");
+  //   $("#develop, #design, #logo, .empty").slideToggle("hidden");
+  // });
   // Animations
   // $("#box-container").addClass("m-scene");
   // $("#logo").addClass("scene__element scene__element--fadeinup");
