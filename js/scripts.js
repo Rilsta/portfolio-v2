66b72ea21e79efd6105d9f5ff549fb23b6496e4f
rilsta <rstarnes9@gmail.com>
2016-05-30 11:10:13 -0700

    add slide toggle to delight and develop

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
 
   //Hidden on page load
   $("#design-content").addClass("dropdown hidden");
+  $("#develop-content").addClass("dropdown hidden");
+  $("#delight-content").addClass("dropdown hidden");
   $("#content-box").addClass("content-box");
 
   //Dropdown hide/show for main menu
   $("#design").click(function() {
     $("#design-content, #develop, #delight, #logo, .empty").slideToggle("hidden");
   });
-  // $("#develop").click(function() {
-  //   $("#develop-content").toggleClass("dropdown");
-  //   $("#design, #delight, #logo, .empty").slideToggle("hidden");
-  // });
-  // $("#delight").click(function() {
-  //   $("#delight-content").toggleClass("dropdown");
-  //   $("#develop, #design, #logo, .empty").slideToggle("hidden");
-  // });
+  $("#develop").click(function() {
+    $("#develop-content, #design, #delight, #logo, .empty").slideToggle("hidden");
+  });
+  $("#delight").click(function() {
+    $("#delight-content, #design, #develop, #logo, .empty").slideToggle("hidden");
+  });
   // Animations
   // $("#box-container").addClass("m-scene");
   // $("#logo").addClass("scene__element scene__element--fadeinup");
